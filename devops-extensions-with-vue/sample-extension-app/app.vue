895a043314c915f445381e66544ab112e85cf66d
Andy Evans <andy.evans@razor.co.uk>
2025-03-04 09:32:51 +0000

    Sample extension app

---
+++ devops-extensions-with-vue/sample-extension-app/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <p>Hello {{userName}}!</p>
+</template>
+
+<script setup lang="ts">
+import * as SDK from 'azure-devops-extension-sdk';
+
+const userName = ref('');
+
+onMounted(async () => {
+  SDK.init();
+  await SDK.ready();
+
+  userName.value = SDK.getUser().displayName;
+});
+</script>
